dc4bccfce7ff582deee3f40a511b470871c6b84c
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-02-27 16:58:02 +0100

    chore(fetch&Lock): update test case
    related to CAM-8775

--- test/workers.test.js
+++ test/workers.test.js
@@ -1,45 +1,55 @@
 const Workers = require('../lib/Workers');
 
 const customWorkersOptions = {
-  path: 'http://localhost:8080/engine-rest/external-task',
+  path: 'http://localhost:XXXX/engine-rest/external-task',
   workerId: 'foobarId',
   maxTasks: 3,
   interval: 100,
   lockDuration: 30000
 };
-const workers = new Workers(customWorkersOptions);
 
-test('throw error: api path wasn\'t passed', () => {
-  expect(() => { new Workers();}).toThrow();
+describe('workers', () => {
+  describe('registerWorker method', () => {
+
+    test('should throw error if api path wasn\'t passed as parameter', () => {
+      expect(() => { new Workers();}).toThrow();
+    });
+
+    const workers = new Workers(customWorkersOptions);
+
+    test('should overwrite default with custom workers config', () =>{
+      expect(workers.options.workerId).toBe(customWorkersOptions.workerId);
+      expect(workers.options.maxTasks).toBe(customWorkersOptions.maxTasks);
+      expect(workers.options.interval).toBe(customWorkersOptions.interval);
+      expect(workers.options.lockDuration).toBe(customWorkersOptions.lockDuration);
+    });
+
+    // Register workers
+    const fooWork = () => 'foobar';
+    const customConfig = { lockDuration: 3000 };
+
+    test('should register worker without custom config ', () => {
+      const barWorker = workers.registerWorker('bar', fooWork);
+      expect(barWorker.handler).not.toBeUndefined();
+    });
+
+    test('should register worker with custom config ', () =>{
+      const fooWorker = workers.registerWorker('bar2', customConfig, fooWork);
+      expect(fooWorker.lockDuration).toBe(customConfig.lockDuration);
+    });
+
+    // REDO test description
+    test('should throw error if work function is not passed', () => {
+      workers.registerWorker('foo', fooWork);
+      expect(() => { workers.registerWorker('foo', fooWork); }).toThrow();
+    });
+  });
+  describe('poll method ', () => {
+    jest.useFakeTimers();
+    const workers = new Workers(customWorkersOptions);
+    test('should call itself again after timeout when no worker registered', () => {
+      expect(setTimeout)
+        .toHaveBeenLastCalledWith(workers.poll, customWorkersOptions.interval );
+    });
+  });
 });
-
-test('overwrite default with custom workers config', () =>{
-  expect(workers.options.workerId).toBe(customWorkersOptions.workerId);
-  expect(workers.options.maxTasks).toBe(customWorkersOptions.maxTasks);
-  expect(workers.options.interval).toBe(customWorkersOptions.interval);
-  expect(workers.options.lockDuration).toBe(customWorkersOptions.lockDuration);
-});
-
-// Register workers
-const fooWork = () => 'foobar';
-const customConfig = { lockDuration: 3000 };
-
-test('registered worker without custom config ', () => {
-  const barWorker = workers.registerWorker('bar', fooWork);
-  expect(barWorker.handler).not.toBeUndefined();
-});
-
-test('throw error: work function wasn\'t passed', () => {
-  workers.registerWorker('foo', fooWork);
-  expect(() => { workers.registerWorker('foo', fooWork); }).toThrow();
-});
-
-test('overwrite default with custom worker config', () =>{
-  const fooWorker = workers.registerWorker('bar2', customConfig, fooWork);
-  expect(fooWorker.lockDuration).toBe(customConfig.lockDuration);
-});
-
-
-// TODO:
-// Test regist / unregister a worker with fetch&lock mock.
-// unregister workers as soon as it is possible.
