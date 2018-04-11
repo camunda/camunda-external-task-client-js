32427cb7c5b75dad5e46a61cffe985153f8efcbb
siffogh <siffogh3@gmail.com>
2018-04-11 11:52:44 +0200

    chore(examples): update example to use latest api

--- examples/granting-loans/example.js
+++ examples/granting-loans/example.js
@@ -1,29 +1,30 @@
-const { Client, logger } = require('camunda-external-task-client-js');
+const { Client, logger, Variables } = require("camunda-external-task-handler-js");
 
 // configuration for the Client:
 //  - 'baseUrl': url to the Workflow Engine
 //  - 'logger': utility to automatically log important events
-const config = { baseUrl: 'http://localhost:8080/engine-rest', use: logger };
+const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
 
 // create a Client instance with custom configuration
 const client = new Client(config);
 
 // create a handler for the task
-const handler = async({ task, taskService }) => {
+const handler = async ({ task, taskService }) => {
   // get task variable 'defaultScore'
-  const defaultScore = task.variables.get('defaultScore');
+  const defaultScore = task.variables.get("defaultScore");
 
-  // set task variable 'creditScores'
-  task.variables.set('creditScores', [defaultScore, 9, 1, 4, 10]);
+  // set process variable 'creditScores'
+  const creditScores = [defaultScore, 9, 1, 4, 10];
+  const processVariables = new Variables().set("creditScores", creditScores);
 
   // complete the task
   try {
-    await taskService.complete(task);
-    console.log('I completed my task successfully!!');
+    await taskService.complete(task, processVariables);
+    console.log("I completed my task successfully!!");
   } catch (e) {
     console.error(`Failed completing my task, ${e}`);
   }
 };
 
 // susbscribe to the topic 'creditScoreChecker' & provide the created handler
-client.subscribe('creditScoreChecker', handler);
+client.subscribe("creditScoreChecker", handler);
