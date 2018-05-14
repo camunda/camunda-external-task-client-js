0271e2a6709de21fa8d4af8b9a26b08daa4ade4d
Seif Eddine Ghezala <siffogh3@gmail.com>
2018-05-14 14:51:40 +0200

    chore(examples): fix npm version in example

--- examples/granting-loans/index.js
+++ examples/granting-loans/index.js
@@ -4,15 +4,11 @@ const {
   Variables
 } = require("camunda-external-task-client-js");
 
-const BearerInterceptor = require("./BearerInterceptor");
-const interceptor = new BearerInterceptor();
-
 // configuration for the Client:
 //  - 'baseUrl': url to the Workflow Engine
 //  - 'logger': utility to automatically log important events
 const config = {
   baseUrl: "http://localhost:8080/engine-rest",
-  interceptors: interceptor,
   use: logger
 };
 
@@ -26,7 +22,9 @@ const handler = async ({ task, taskService }) => {
 
   // set process variable 'creditScores'
   const creditScores = [defaultScore, 9, 1, 4, 10];
-  const processVariables = new Variables().set("creditScores", creditScores);
+  const processVariables = new Variables()
+    .set("creditScores", creditScores)
+    .set("bar", new Date());
 
   // complete the task
   try {
@@ -39,3 +37,8 @@ const handler = async ({ task, taskService }) => {
 
 // susbscribe to the topic 'creditScoreChecker' & provide the created handler
 client.subscribe("creditScoreChecker", handler);
+
+client.subscribe("requestRejecter", async ({ task, taskService }) => {
+  console.log(task.variables.get("bar"));
+  console.log(task.variables.get("creditScores"));
+});
